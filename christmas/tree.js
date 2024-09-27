4467009a3a6c4ce12ead23d36839594abecc7a30
FATHIMA FIDHA <spk610520@gmail.com>
2024-09-27 11:08:13 +0530

    JAVASCRIPT-PATTERNS

---
+++ christmas/tree.js
@@ -0,0 +1,37 @@
+var a=""
+    
+for(i=1;i<=6;i++)
+{
+    a=""
+    for(j=5;j>=i;j--)
+    {
+        a=a+"  "
+    }
+    for(k=1;k<=i;k++)
+    {
+        a=a+"  * "
+    }
+    console.log(a)
+}
+for(i=1;i<=6;i++)
+{
+    a=""
+    for(j=5;j>=i;j--)
+    {
+        a=a+"  "
+    }
+    for(k=1;k<=i;k++)
+    {
+        a=a+"  * "
+    }
+    console.log(a)
+}
+for(i=1;i<=5;i++){
+    a=""
+    for(j=1;j<=3;j++){
+        a=a+"   "
+    }
+    for(k=1;k<=2;k++)
+    {a=a+" * "}
+    console.log(a)
+}
