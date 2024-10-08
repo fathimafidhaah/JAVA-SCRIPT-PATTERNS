949151d402f98cf4ac5d66064d121e26354da8bb
FATHIMA FIDHA <spk610520@gmail.com>
2024-10-08 10:33:42 +0530

    JAVASCRIPT-PATTERNS

---
+++ SPpattern/dp.js
@@ -0,0 +1,30 @@
+var a=""
+    
+for(i=1;i<=5;i++)
+{
+    a=""
+    for(j=5;j>=i;j--)
+    {
+        a=a+"  "
+    }
+    for(k=1;k<=i;k++)
+    {
+        a=a+"  * "
+    }
+    console.log(a)
+}
+for(i=1;i<=6;i++)
+    {
+        a="" 
+      for(j=2;j<=i;j++)
+      {
+        a=a+"  "  
+      }
+    for(k=6;k>=i;k--)
+        {
+            a=a+"  * "
+        }
+      
+      console.log(a);
+    }
+    
