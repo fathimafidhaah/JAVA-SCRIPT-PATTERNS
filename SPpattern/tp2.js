ecb38f2c560055f175301c899139d92f72ccef42
FATHIMA FIDHA <spk610520@gmail.com>
2024-10-21 11:18:35 +0530

    JAVASCRIPT-PATTERNS

---
+++ SPpattern/tp2.js
@@ -0,0 +1,21 @@
+var a=""
+
+
+for(i=1;i<=5;i++)
+{
+    a=""
+    for(j=1;j<=i;j++)
+    {
+        a=a+" * "
+    }
+    console.log(a)
+}
+for(i=1;i<=5;i++)
+    {
+        a=""
+        for(j=5;j>=i;j--)
+        {
+            a=a+" * "
+        }
+        console.log(a)
+    }
